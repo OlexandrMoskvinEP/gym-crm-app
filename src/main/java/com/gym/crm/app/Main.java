64db816567cc71115579aace799ca82457d18758
Alex Moskvin <olexandr_moskvin@epam.com>
2025-06-22 13:12:40 +0300

    GCA-21 init spring core application

--- src/main/java/com/gym/crm/app/Main.java
+++ src/main/java/com/gym/crm/app/Main.java
@@ -1,6 +1,11 @@
 package com.gym.crm.app;
+import com.gym.crm.app.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        context.close();
     }
 }
---
+++ src/main/java/com/gym/crm/app/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.gym.crm.app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.gym.crm.app")
+public class AppConfig {
+}
