c16998804262201fb9b6d36dd497773da410aa43
Alex Moskvin <olexandr_moskvin@epam.com>
2025-07-03 16:57:31 +0300

    GCA-58 start adapting user entity

--- src/main/java/com/gym/crm/app/domain/model/User.java
+++ src/main/java/com/gym/crm/app/domain/model/User.java
@@ -1,26 +1,49 @@
 package com.gym.crm.app.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
 @Getter
-@ToString
 @EqualsAndHashCode
+@ToString
 @SuperBuilder(toBuilder = true)
-@AllArgsConstructor
-@NoArgsConstructor(force = true)
+@NoArgsConstructor
 public class User {
-    private final String firstName;
-    private final String lastName;
-    private final String username;
-    private final String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 20)
+    private String firstName;
+
+    @Column(nullable = false, length = 20)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @ToString.Exclude
+    @Column(nullable = false)
+    private String password;
+
     @JsonProperty("isActive")
-    private final boolean isActive;
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
 
     @JsonProperty("isActive")
     public boolean getIsActive() {
