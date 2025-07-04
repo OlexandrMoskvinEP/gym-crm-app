50bde336fa1cd8684cb17c6a9a894e176d0bf9b6
Alex Moskvin <olexandr_moskvin@epam.com>
2025-07-04 22:16:37 +0300

    GCA-59 implement liquidBase migration

--- src/main/java/com/gym/crm/app/config/AppConfig.java
+++ src/main/java/com/gym/crm/app/config/AppConfig.java
@@ -8,6 +8,7 @@ import com.gym.crm.app.config.hibernate.HibernateConfig;
 import com.gym.crm.app.domain.model.Trainee;
 import com.gym.crm.app.domain.model.Trainer;
 import com.gym.crm.app.domain.model.Training;
+import liquibase.integration.spring.SpringLiquibase;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.sql.DataSource;
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
@@ -58,5 +60,13 @@ public class AppConfig {
     @Bean
     public SecureRandom secureRandom() {
         return new SecureRandom();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/changelog-master.xml");
+        return liquibase;
     }
 }
