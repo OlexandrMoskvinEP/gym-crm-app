e983a953c86a263e76ede5222ceb224797340fbc
Alex Moskvin <olexandr_moskvin@epam.com>
2025-07-12 17:07:44 +0300

    GCA-64 create tests for new UserProfile

--- src/test/java/com/gym/crm/app/service/common/UserProfileServiceTest.java
+++ src/test/java/com/gym/crm/app/service/common/UserProfileServiceTest.java
@@ -1,33 +1,44 @@
 package com.gym.crm.app.service.common;
 
-import com.gym.crm.app.data.TestData;
+import com.gym.crm.app.domain.model.Trainee;
+import com.gym.crm.app.domain.model.Trainer;
+import com.gym.crm.app.domain.model.User;
 import com.gym.crm.app.repository.TraineeRepository;
 import com.gym.crm.app.repository.TrainerRepository;
+import com.gym.crm.app.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserProfileServiceTest {
-    private final TestData data = new TestData();
+    private static final Trainee TRAINEE = Trainee.builder().user(User.builder().firstName("Alice").lastName("Smith").username("Alice.Smith").build()).build();
+    private static final Trainer TRAINER = Trainer.builder().user(User.builder().firstName("Sophie").lastName("Taylor").username("Sophie.Taylor").build()).build();
 
     @Mock
     TraineeRepository traineeRepository;
     @Mock
     TrainerRepository trainerRepository;
+    @Mock
+    UserRepository userRepository;
     @InjectMocks
     private UserProfileService service;
 
     @Test
     void shouldCreateCorrectUsername() {
-        when(traineeRepository.findAll()).thenReturn(data.getTrainees());
-        when(trainerRepository.findAll()).thenReturn(data.getTrainers());
+        when(traineeRepository.findAll()).thenReturn(List.of(TRAINEE));
+        when(trainerRepository.findAll()).thenReturn(List.of(TRAINER));
 
         String traineeUsernameExisting = "Alice.Smith";
         String traineeUsernameGenerated = service.createUsername("Alice", "Smith");
@@ -42,12 +53,21 @@ class UserProfileServiceTest {
         assertNotEquals(trainerUsernameExisting, trainerUsernameGenerated);
     }
 
-    //todo 2 tests
     @Test
     void changePassword() {
+        doNothing().when(userRepository).updatePassword(anyString(), anyString());
+
+        userRepository.updatePassword("username", "qwerty321");
+
+        verify(userRepository).updatePassword("username", "qwerty321");
     }
 
     @Test
     void changeStatus() {
+        doNothing().when(userRepository).changeStatus(anyString());
+
+        userRepository.changeStatus("username");
+
+        verify(userRepository).changeStatus("username");
     }
 }
