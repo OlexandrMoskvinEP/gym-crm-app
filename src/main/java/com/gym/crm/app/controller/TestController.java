7e6d1e8c40bc8cedf9e0856b241321ba5bbb9ace
Alex Moskvin <olexandr_moskvin@epam.com>
2025-07-15 16:46:58 +0300

    GCA-99  create swagger file

--- src/main/java/com/gym/crm/app/controller/TestController.java
+++ src/main/java/com/gym/crm/app/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.gym.crm.app.controller;
 
+import com.gym.dto.TraineeCreateRequest;
+import com.gym.dto.TraineeRegistrationResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,5 +12,5 @@ public class TestController {
     @ResponseBody
     public String hello() {
         return "Hello, Welcome to  Gym CRM!";
-    }
+       }
 }
