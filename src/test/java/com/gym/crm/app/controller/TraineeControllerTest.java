1f34150e93baa948b63041befeaa8c9756978e7b
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-07-20 14:06:58 +0300

    GCA-100 create test class

---
+++ src/test/java/com/gym/crm/app/controller/TraineeControllerTest.java
@@ -0,0 +1,40 @@
+package com.gym.crm.app.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TraineeControllerTest {
+
+    @Test
+    void registerTrainee() {
+    }
+
+    @Test
+    void getTraineeProfile() {
+    }
+
+    @Test
+    void updateTraineeProfile() {
+    }
+
+    @Test
+    void deleteTraineeProfile() {
+    }
+
+    @Test
+    void getAvailableTrainers() {
+    }
+
+    @Test
+    void updateTraineeTrainers() {
+    }
+
+    @Test
+    void getTraineeTrainings() {
+    }
+
+    @Test
+    void changeTraineeActivationStatus() {
+    }
+}
