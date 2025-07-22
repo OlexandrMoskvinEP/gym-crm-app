01ecc012c06bb07201570e41ea2890461f89ca38
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-07-22 14:59:14 +0300

    GCA-102 add necessary exceptions

---
+++ src/main/java/com/gym/crm/app/exception/AuthentificationException.java
@@ -0,0 +1,7 @@
+package com.gym.crm.app.exception;
+
+public class AuthentificationException extends RuntimeException {
+    public AuthentificationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/gym/crm/app/exception/AuthorisationException.java
@@ -0,0 +1,7 @@
+package com.gym.crm.app.exception;
+
+public class AuthorisationException extends RuntimeException {
+    public AuthorisationException(String message) {
+        super(message);
+    }
+}
