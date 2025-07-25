a2092b8d69b6999ea888aae98ecfd4ead2ff0ce0
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-07-25 18:17:54 +0300

    GCA-105 Fix passwords

--- src/main/java/com/gym/crm/app/Main.java
+++ src/main/java/com/gym/crm/app/Main.java
@@ -4,6 +4,7 @@ import com.gym.crm.app.config.AppConfig;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -25,6 +26,10 @@ public class Main {
         DispatcherServlet servlet = new DispatcherServlet(appContext);
         Tomcat.addServlet(webContext, "dispatcher", servlet);
         webContext.addServletMappingDecoded("/", "dispatcher");
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hash = "$2a$10$QXB1QANM/95QETWLqUQAuOEOyw.B/XBWs/x2DGHk5/G8wjskmifiq";
+        System.out.println(encoder.matches("qwerty1234", hash));
 
         tomcat.start();
         tomcat.getServer().await();
