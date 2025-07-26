9b98f2cddd0c8329b9f88fabc3ab6be5e364565b
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-07-26 16:21:25 +0300

    GCA-106 implement training controller docs and UI

--- src/main/java/com/gym/crm/app/controller/TrainingController.java
+++ src/main/java/com/gym/crm/app/controller/TrainingController.java
@@ -1,8 +1,14 @@
 package com.gym.crm.app.controller;
 
 import com.gym.crm.app.facade.GymFacade;
+import com.gym.crm.app.rest.ErrorResponse;
 import com.gym.crm.app.rest.TrainingCreateRequest;
 import com.gym.crm.app.rest.TrainingTypeGetResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +24,25 @@ import static com.gym.crm.app.controller.ApiConstants.ROOT_PATH;
 public class TrainingController {
     private GymFacade facade;
 
+    @Operation(
+            summary = "Get training types",
+            description = "Retrieves list of all available training types",
+            tags = {"Training services"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operation successful"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ),
+            @ApiResponse(
+                    description = "Unexpected error"
+            )
+    })
     @GetMapping("/training-types")
     public ResponseEntity<TrainingTypeGetResponse> getTrainingTypes() {
         log.info("Get training types request");
@@ -27,6 +52,38 @@ public class TrainingController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "Add new training",
+            description = "Creates a new training session between trainee and trainer",
+            tags = {"Training services"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operation successful"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid input data",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ), @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorised access",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+    ), @ApiResponse(
+            responseCode = "404",
+            description = "Trainee or trainer not found",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+    ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ),
+            @ApiResponse(
+                    description = "Unexpected error"
+            )
+    })
     @PostMapping("/trainings")
     public ResponseEntity<Void> addTraining(TrainingCreateRequest trainingCreateRequest) {
         log.info("Add training attempt: traineeUsername={}, trainerUsername={}",
