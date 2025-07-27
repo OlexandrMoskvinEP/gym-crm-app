0f73d9acb50e3d0db39b812d92e1cb130a2789ef
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-07-27 16:23:58 +0300

    GCA-105-1 fix tests

--- src/test/java/com/gym/crm/app/logging/TransactionLoggingFilterTest.java
+++ src/test/java/com/gym/crm/app/logging/TransactionLoggingFilterTest.java
@@ -1,6 +1,5 @@
 package com.gym.crm.app.logging;
 
-import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
