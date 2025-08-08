1d201e83022ef3f83c97538dfcd7a655e59ddd96
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-08-08 21:17:52 +0300

    GCA-146 create refreshToken table and entity, configure liquibase

---
+++ src/main/java/com/gym/crm/app/domain/model/RefreshToken.java
@@ -0,0 +1,41 @@
+package com.gym.crm.app.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@EqualsAndHashCode
+@ToString
+@Builder(toBuilder = true)
+@Table(name = "refresh_token")
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+    @Id
+    @Column
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "issued_at", nullable = false)
+    private Instant issuedAt;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+}
