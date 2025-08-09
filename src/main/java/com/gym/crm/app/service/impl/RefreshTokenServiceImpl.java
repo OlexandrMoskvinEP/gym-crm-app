35b51e4a6100d0c6f7db9f2f99a7f17f505e067a
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-08-09 12:44:07 +0300

    GCA-146 create refreshToken service and tests

---
+++ src/main/java/com/gym/crm/app/service/impl/RefreshTokenServiceImpl.java
@@ -0,0 +1,75 @@
+package com.gym.crm.app.service.impl;
+
+import com.gym.crm.app.domain.model.RefreshToken;
+import com.gym.crm.app.exception.CoreServiceException;
+import com.gym.crm.app.repository.RefreshTokenRepository;
+import com.gym.crm.app.service.RefreshTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    public String saveRefreshToken(RefreshToken refreshToken, Long userId) {
+        String rawToken = refreshToken.getToken();
+        String hashedToken = hashToken(rawToken);
+
+        RefreshToken tokenToSave = refreshToken.toBuilder()
+                .token(hashedToken)
+                .userId(userId).build();
+
+        refreshTokenRepository.save(tokenToSave);
+
+        return rawToken;
+    }
+
+    @Override
+    public Optional<Long> validateAndGetUserId(String refreshToken) {
+        String hashedToken = hashToken(refreshToken);
+
+        return refreshTokenRepository.findById(hashedToken)
+                .filter(rt -> rt.getExpiresAt().isAfter(Instant.now()))
+                .map(RefreshToken::getUserId);
+    }
+
+    @Override
+    public void delete(String refreshToken) {
+        refreshTokenRepository.deleteById(hashToken(refreshToken));
+    }
+
+    @Override
+    public void deleteAllByUserId(Long userId) {
+        refreshTokenRepository.deleteByUserId(userId);
+
+    }
+
+    private String hashToken(String token) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(token.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new CoreServiceException(e.getMessage());
+        }
+    }
+}
