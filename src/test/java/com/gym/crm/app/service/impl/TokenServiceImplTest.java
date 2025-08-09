f4fcfde12ed6f620479bdeb64af41dd9c702844d
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-08-09 16:28:49 +0300

    GCA-146 add TokenService

---
+++ src/test/java/com/gym/crm/app/service/impl/TokenServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.gym.crm.app.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+class TokenServiceImplTest {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void refresh() {
+    }
+}
